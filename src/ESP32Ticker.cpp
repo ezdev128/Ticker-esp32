b1c951b8626c63a11da6f65f35f3a408684ecbd8
K <ezdev128@users.noreply.github.com>
2022-08-15 12:01:21 +0300

    added multi ticker ability

--- src/ESP32Ticker.cpp
+++ src/ESP32Ticker.cpp
@@ -1,103 +1,279 @@
-/* 
-  ESP32Ticker.cpp - esp32 library that calls functions periodically
-
-	Copyright (c) 2017 Bert Melis. All rights reserved.
-	
-	Based on the original work of:
-  Copyright (c) 2014 Ivan Grokhotkov. All rights reserved.
-	The original version is part of the esp8266 core for Arduino environment.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+#include <string>
+#include <esp32-hal-log.h>
 #include "ESP32Ticker.h"
 
-Ticker::Ticker() : _timer(nullptr) {
+static const char *tag = "ESP32Ticker";
+
+Ticker::Ticker(bool useMultiTicker) {
+    is_multi_ticker = useMultiTicker;
+    if (esp_timer_init() != ESP_OK) {
+        ESP_LOGD(tag, "esp_timer_init() failed");
+    }
 }
 
 Ticker::~Ticker() {
   detach();
 }
 
-void Ticker::_attach_ms(uint32_t milliseconds, bool repeat, callback_with_arg_t callback, uintptr_t arg) {
-  esp_timer_create_args_t _timerConfig;
-  _timerConfig.arg = reinterpret_cast<void*>(arg);
-  _timerConfig.callback = callback;
-  _timerConfig.dispatch_method = ESP_TIMER_TASK;
-  _timerConfig.name = "Ticker";
-  if (_timer) {
-    esp_timer_stop(_timer);
-    esp_timer_delete(_timer);
-  }
-  esp_timer_create(&_timerConfig, &_timer);
-  if (repeat) {
-    esp_timer_start_periodic(_timer, milliseconds * 1000);
-  } else {
-    esp_timer_start_once(_timer, milliseconds * 1000);
-  }
+UN void Ticker::enable_multiticker() {
+    is_multi_ticker = true;
 }
 
-uint32_t Ticker::_to_ms(float seconds) {
+UN void Ticker::_attach_ms(uint32_t milliseconds, bool repeat, callback_with_arg_t callback, uintptr_t arg) {
+    std::string tickerName = tag;
+    if (is_multi_ticker) {
+        tickerName += "." + std::to_string((unsigned long) (esp_timer_get_time() / 1000ULL));
+    }
+
+    auto * to = new ticker_object_t{
+            .timer_config = {
+                    .callback = callback,
+                    .arg = reinterpret_cast<void *>(arg),
+                    .dispatch_method = ESP_TIMER_TASK,
+                    .name = tickerName.c_str(),
+            },
+            .timer_handle = is_multi_ticker ? nullptr : _timer_handle,
+            .repeatable = repeat,
+            .milliseconds = milliseconds,
+    };
+
+    detach(to->timer_handle);
+    if (esp_timer_create(&to->timer_config, &to->timer_handle) != ESP_OK) {
+        ESP_LOGD(tag, "esp_timer_create() failed");
+    }
+
+    if (repeat) {
+        if (esp_timer_start_periodic(to->timer_handle, milliseconds * 1000) != ESP_OK) {
+            ESP_LOGD(tag, "esp_timer_start_periodic() failed");
+        }
+    } else {
+        if (esp_timer_start_once(to->timer_handle, milliseconds * 1000) != ESP_OK) {
+            ESP_LOGD(tag, "esp_timer_start_once() failed");
+        }
+    }
+
+    if (!is_multi_ticker) {
+        _timer_handle = to->timer_handle;
+        tickers_objects.clear();
+    }
+
+    tickers_objects.insert(tickers_objects.end(), *to);
+}
+
+uint32_t Ticker::to_ms(float seconds) {
     return (uint32_t)(seconds * 1000);
 }
 
-__attribute__((unused)) void Ticker::detach() {
-  if (_timer) {
-    esp_timer_stop(_timer);
-    esp_timer_delete(_timer);
-    _timer = nullptr;
-  }
+UN void Ticker::attach(float seconds, Ticker::callback_t callback) {
+    _attach_ms(to_ms(seconds), true, reinterpret_cast<callback_with_arg_t>(callback), 0);
 }
 
-__attribute__((unused)) void Ticker::attach(float seconds, Ticker::callback_t callback) {
-    _attach_ms(_to_ms(seconds), true, reinterpret_cast<callback_with_arg_t>(callback), 0);
+TARG UN void Ticker::attach(float seconds, void (*callback)(arg_t), arg_t arg) {
+    static_assert(sizeof(arg_t) <= sizeof(uintptr_t), "attach() callback argument size must be <= 32 bytes");
+    _attach_ms(to_ms(seconds), true, reinterpret_cast<callback_with_arg_t>(callback), (uintptr_t)arg);
 }
 
-
-__attribute__((unused)) void Ticker::attach_ms(uint32_t milliseconds, Ticker::callback_t callback) {
+UN void Ticker::attach_ms(uint32_t milliseconds, Ticker::callback_t callback) {
     _attach_ms(milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), 0);
 }
 
-template<typename TArg>
-__attribute__((unused)) void Ticker::attach(float seconds, void (*callback)(TArg), TArg arg) {
-    static_assert(sizeof(TArg) <= sizeof(uintptr_t), "attach() callback argument size must be <= 32 bytes");
-    _attach_ms(_to_ms(seconds), true, reinterpret_cast<callback_with_arg_t>(callback), (uintptr_t)arg);
-}
-
-template<typename TArg>
-__attribute__((unused)) void Ticker::attach_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg) {
-    static_assert(sizeof(TArg) <= sizeof(uintptr_t), "attach_ms() callback argument size must be <= 32 bytes");
+TARG UN void Ticker::attach_ms(uint32_t milliseconds, void (*callback)(arg_t), arg_t arg) {
+    static_assert(sizeof(arg_t) <= sizeof(uintptr_t), "attach_ms() callback argument size must be <= 32 bytes");
     _attach_ms(milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), (uintptr_t)arg);
 }
 
-__attribute__((unused)) void Ticker::once(float seconds, Ticker::callback_t callback) {
-    _attach_ms(_to_ms(seconds), false, reinterpret_cast<callback_with_arg_t>(callback), 0);
+UN void Ticker::once(float seconds, Ticker::callback_t callback) {
+    _attach_ms(to_ms(seconds), false, reinterpret_cast<callback_with_arg_t>(callback), 0);
 }
 
-__attribute__((unused)) void Ticker::once_ms(uint32_t milliseconds, Ticker::callback_t callback) {
+UN void Ticker::once_ms(uint32_t milliseconds, Ticker::callback_t callback) {
     _attach_ms(milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), 0);
 }
 
-template<typename TArg>
-__attribute__((unused)) void Ticker::once(float seconds, void (*callback)(TArg), TArg arg) {
-    static_assert(sizeof(TArg) <= sizeof(uintptr_t), "attach() callback argument size must be <= 32 bytes");
-    _attach_ms(_to_ms(seconds), false, reinterpret_cast<callback_with_arg_t>(callback), (uintptr_t)(arg));
+TARG UN void Ticker::once(float seconds, void (*callback)(arg_t), arg_t arg) {
+    static_assert(sizeof(arg_t) <= sizeof(uintptr_t), "attach() callback argument size must be <= 32 bytes");
+    _attach_ms(to_ms(seconds), false, reinterpret_cast<callback_with_arg_t>(callback), (uintptr_t)(arg));
 }
 
-template<typename TArg>
-__attribute__((unused)) void Ticker::once_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg) {
-    static_assert(sizeof(TArg) <= sizeof(uintptr_t), "attach_ms() callback argument size must be <= 32 bytes");
+TARG UN void Ticker::once_ms(uint32_t milliseconds, void (*callback)(arg_t), arg_t arg) {
+    static_assert(sizeof(arg_t) <= sizeof(uintptr_t), "attach_ms() callback argument size must be <= 32 bytes");
     _attach_ms(milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), (uintptr_t)(arg));
+}
+
+UN bool Ticker::detach() {
+    if (is_multi_ticker) {
+        for (auto t : tickers_objects) {
+            if (!detach(t)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return detach(_timer_handle);
+}
+
+UN bool Ticker::detach(esp_timer_handle_t &timer_handle) {
+    if (esp_timer_stop(timer_handle) == ESP_ERR_INVALID_STATE
+        || esp_timer_delete(timer_handle) == ESP_ERR_INVALID_STATE) {
+        return false;
+    }
+    timer_handle = nullptr;
+    return true;
+}
+
+UN bool Ticker::detach(Ticker::ticker_object_t ticker_object) {
+    if (ticker_object.timer_handle == nullptr) {
+        return false;
+    }
+
+    detach(ticker_object.timer_handle);
+    auto tickerIdx = get_ticker_object_index(ticker_object);
+
+    if (tickerIdx < 0) {
+        return false;
+    }
+
+    tickers_objects.erase(tickers_objects.begin() + tickerIdx);
+    return true;
+}
+
+UN bool Ticker::detach(Ticker::callback_t callback) {
+    return detach(find_ticker_object(callback));
+}
+
+UN bool Ticker::active() {
+    return is_multi_ticker ? !tickers_objects.empty() : _timer_handle != nullptr && esp_timer_is_active(_timer_handle);
+}
+
+UN bool Ticker::active(esp_timer_handle_t timer_handle) {
+    if (is_multi_ticker) {
+        if (timer_handle == nullptr) {
+            return false;
+        }
+        return esp_timer_is_active(timer_handle);
+    } else {
+        if (_timer_handle == nullptr) {
+            return false;
+        }
+        return esp_timer_is_active(_timer_handle);
+    }
+}
+
+UN bool Ticker::active(Ticker::callback_t callback) {
+    if (is_multi_ticker) {
+        Ticker::ticker_object_t tickerObject = find_ticker_object(callback);
+        if (tickerObject.timer_handle == nullptr) {
+            return false;
+        }
+        return esp_timer_is_active(tickerObject.timer_handle);
+    }
+    return esp_timer_is_active(_timer_handle);
+}
+
+UN int Ticker::get_ticker_object_index(Ticker::ticker_object_t ticker_object) {
+    int i=0;
+    for (auto t : tickers_objects) {
+        if (t.timer_handle == nullptr) {
+            i++;
+            continue;
+        }
+
+        if (t.timer_config.callback == ticker_object.timer_config.callback) {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+UN Ticker::ticker_object_t Ticker::find_ticker_object(Ticker::ticker_object_t ticker_object) {
+    if (tickers_objects.empty() || ticker_object.timer_handle == nullptr){
+        return ticker_object_t{};
+    }
+
+    for (auto t : tickers_objects) {
+        if (t.timer_handle == nullptr) {
+            continue;
+        }
+
+        if (t.timer_handle == ticker_object.timer_handle) {
+            return ticker_object;
+        }
+    }
+
+    return ticker_object_t{};
+}
+
+UN Ticker::ticker_object_t Ticker::find_ticker_object(Ticker::callback_with_arg_t callback) {
+    static Ticker::ticker_object_t ticker_object;
+
+    if (tickers_objects.empty()){
+        return ticker_object_t{};
+    }
+
+    for (auto t : tickers_objects) {
+        if (t.timer_handle == nullptr) {
+            continue;
+        }
+
+        if (t.timer_config.callback == *callback) {
+            ticker_object = t;
+            break;
+        }
+    }
+
+    return ticker_object;
+}
+
+
+UN Ticker::ticker_object_t Ticker::find_ticker_object(Ticker::callback_t callback) {
+    return find_ticker_object(reinterpret_cast<Ticker::callback_with_arg_t>(callback));
+}
+
+
+UN bool Ticker::has_ticker_object(Ticker::ticker_object_t ticker_object) {
+    return find_ticker_object(ticker_object).timer_handle != nullptr;
+}
+
+UN bool Ticker::has_ticker_object(Ticker::callback_with_arg_t callback) {
+    auto ticker_object = find_ticker_object(callback);
+    return ticker_object.timer_handle != nullptr
+        && ticker_object.timer_config.callback == callback;
+}
+
+UN bool Ticker::has_ticker_object(Ticker::callback_t callback) {
+    return has_ticker_object(reinterpret_cast<Ticker::callback_with_arg_t>(callback));
+}
+
+UN int Ticker::get_tickers_count() {
+    if (!is_multi_ticker) {
+        return active(_timer_handle) ? 1 : 0;
+    }
+
+    int cnt = 0;
+    for (auto t : tickers_objects) {
+        if (t.timer_handle == nullptr) {
+            continue;
+        }
+        cnt++;
+    }
+    return cnt;
+}
+
+std::vector<Ticker::ticker_task_object *> Ticker::get_tickers() {
+    std::vector<Ticker::ticker_task_object *> ticker_objects;
+
+    for (auto t : tickers_objects) {
+        if (t.timer_handle == nullptr) {
+            continue;
+        }
+
+        ticker_objects.insert(ticker_objects.end(),
+                              new ticker_task_object{
+                                  .callback = t.timer_config.callback,
+                                  .repeatable = t.repeatable,
+                                  .milliseconds = t.milliseconds,
+                              });
+    }
+    return ticker_objects;
 }
--- src/ESP32Ticker.h
+++ src/ESP32Ticker.h
@@ -1,4 +1,9 @@
 /* 
+
+  2022-08.14 Added multiticker functionality
+
+  -------------------------------------------------
+  Code is based on:
   Ticker.h - esp32 library that calls functions periodically
 
   Copyright (c) 2017 Bert Melis. all rights reserved
@@ -20,10 +25,17 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef TICKER_H
-#define TICKER_H
+#ifndef __ESP332_TICKER_H__
+#define __ESP332_TICKER_H__
 
-#include <esp32-hal-log.h>
+#include <vector>
+
+#ifndef TARG
+#define TARG template<typename arg_t>
+#endif
+#ifndef UN
+#define UN __attribute__((unused))
+#endif
 
 extern "C" {
 	#include "esp_timer.h"
@@ -32,34 +44,70 @@ extern "C" {
 class Ticker
 {
 public:
-	Ticker();
-	~Ticker();
 	typedef void (*callback_t)();
 	typedef void (*callback_with_arg_t)(void*);
+    struct ticker_task_object {
+        callback_with_arg_t callback;
+        bool repeatable;
+        uint32_t milliseconds;
+    };
 
-    __attribute__((unused)) void attach(float seconds, callback_t callback);
-    __attribute__((unused)) void attach_ms(uint32_t milliseconds, callback_t callback);
-	template<typename TArg>
-    __attribute__((unused)) void attach(float seconds, void (*callback)(TArg), TArg arg);
-	template<typename TArg>
-    __attribute__((unused)) void attach_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg);
+    explicit Ticker(bool = false);
+    ~Ticker();
 
-    __attribute__((unused)) void once(float seconds, callback_t callback);
-    template<typename TArg>
-    __attribute__((unused)) void once(float seconds, void (*callback)(TArg), TArg arg);
-    __attribute__((unused)) void once_ms(uint32_t milliseconds, callback_t callback);
-	template<typename TArg>
-    __attribute__((unused)) void once_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg);
+    UN void enable_multiticker();
 
-    __attribute__((unused)) void detach();
-    __attribute__((unused)) bool active();
+    UN void attach(float seconds, callback_t callback);
+    TARG UN void attach(float seconds, void (*callback)(arg_t), arg_t arg);
 
-protected:	
-    void _attach_ms(uint32_t milliseconds, bool repeat, callback_with_arg_t callback, uintptr_t arg);
-    static uint32_t _to_ms(float seconds);
+    UN void attach_ms(uint32_t milliseconds, callback_t callback);
+    TARG UN void attach_ms(uint32_t milliseconds, void (*callback)(arg_t), arg_t arg);
+
+    UN void once(float seconds, callback_t callback);
+    TARG UN void once(float seconds, void (*callback)(arg_t), arg_t arg);
+
+    UN void once_ms(uint32_t milliseconds, callback_t callback);
+    TARG UN void once_ms(uint32_t milliseconds, void (*callback)(arg_t), arg_t arg);
+
+    UN bool detach();
+    UN bool detach(Ticker::callback_t);
+
+    UN bool active();
+    UN bool active(Ticker::callback_t);
+
+    UN int get_tickers_count();
+    UN std::vector<ticker_task_object *> get_tickers();
 
 protected:
-	esp_timer_handle_t _timer;
+    struct ticker_object_t {
+        esp_timer_create_args_t timer_config;
+        esp_timer_handle_t timer_handle;
+        bool repeatable;
+        uint32_t milliseconds;
+    };
+
+protected:
+    esp_timer_handle_t _timer_handle = {nullptr};
+    void _attach_ms(uint32_t milliseconds, bool repeat, callback_with_arg_t callback, uintptr_t arg);
+    UN bool detach(Ticker::ticker_object_t);
+    UN static bool detach(esp_timer_handle_t &);
+    UN bool active(esp_timer_handle_t);
+
+
+private:
+    bool is_multi_ticker = false;
+    std::vector<ticker_object_t> tickers_objects = {};
+    static uint32_t to_ms(float seconds);
+
+    int get_ticker_object_index(ticker_object_t);
+    ticker_object_t find_ticker_object(Ticker::ticker_object_t);
+    ticker_object_t find_ticker_object(Ticker::callback_with_arg_t);
+    ticker_object_t find_ticker_object(Ticker::callback_t);
+
+    UN bool has_ticker_object(Ticker::ticker_object_t);
+    UN bool has_ticker_object(Ticker::callback_with_arg_t);
+    UN bool has_ticker_object(Ticker::callback_t);
+
 };
 
-#endif // TICKER_H
+#endif // __ESP332_TICKER_H__
